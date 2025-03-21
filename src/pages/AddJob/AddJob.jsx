eeb7f25d41abed207f71bd63609d63d6173bdf0b
Moniruzzaman Shawon <m.zaman.djp@gmail.com>
2025-03-21 18:34:54 +0600

    add job UI added

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -1,8 +1,10 @@
+
 import React from 'react';
 
 const AddJob = () => {
     return (
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
             <form className="card-body">
                 {/* Job Title */}
                 <div className="form-control">
@@ -11,19 +13,82 @@ const AddJob = () => {
                     </label>
                     <input type="text" name='title' placeholder="Job Title" className="input input-bordered" required />
                 </div>
+                {/* Job Location */}
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text">Password</span>
+                        <span className="label-text">Location</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <input type="location" name='location' placeholder="Job-Location" className="input input-bordered" required />
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                     </label>
+                </div>
+                {/* Job Type */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Type</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a Job Type</option>
+                        <option>Full-time</option>
+                        <option>Intern</option>
+                        <option>Part-time</option>
+                    </select>
+
+                </div>
+                {/* Job Field */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Field</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a Job Field</option>
+                        <option>Engineering</option>
+                        <option>Marketing</option>
+                        <option>Teaching</option>
+                        <option>Finance</option>
+                    </select>
+
+                </div>
+                {/* {Salary Range} */}
+                <div className='grid gird-cols-1 lg:grid-cols-3 gap-4'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Min</span>
+                        </label>
+                        <input type="text" name='min' placeholder="min" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Max</span>
+                        </label>
+                        <input type="text" name='max' placeholder="max" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Currency</span>
+                        </label>
+                        <select className="select select-ghost w-full max-w-xs">
+                            <option disabled selected>Pick a Job Field</option>
+                            <option>BDT</option>
+                            <option>USD</option>
+                            <option>INR</option>
+
+                        </select>
+
+                    </div>
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Login</button>
                 </div>
             </form>
+        </div>
         </div>
     );
 };
